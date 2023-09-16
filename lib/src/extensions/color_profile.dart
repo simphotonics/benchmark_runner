523af454bdb9a772f6fa0e9a7a2bec58c7d94902
simphotonics <git@simphotonics.com>
2023-09-16 08:34:44 +0100

    Added class ColorProfile.

---
+++ lib/src/extensions/color_profile.dart
@@ -0,0 +1,44 @@
+import 'package:ansi_modifier/ansi_modifier.dart';
+
+/// Defines styles and colors used to print benchmark reports to a terminal.
+extension ColorProfile on Ansi {
+
+  /// Style of an error message
+  static Ansi error = Ansi.red;
+
+  /// Style of a group label.
+  static Ansi group = Ansi.defaultFont;
+
+  /// Style of a benchmark label.
+  static Ansi benchmark = Ansi.defaultFg;
+
+  /// Style of an asynchronous benchmark label.
+  static Ansi asyncBenchmark = Ansi.defaultFont;
+
+  /// Style used to print a sample mean.
+  static Ansi mean = Ansi.green;
+
+  /// Style used to print a sample median.
+  static Ansi median = Ansi.blue;
+
+  /// Style used to color the histogram block containing the mean value.
+  static Ansi meanHistogramBlock = mean;
+
+  /// Style used to print the histogram block containing the median value.
+  static Ansi medianHistogramBlock = median;
+
+  /// Style used to print the histogram block containing mean and median.
+  static Ansi meanMedianHistogramBlock = Ansi.cyan;
+
+  /// Style used to print dimmed console messages.
+  static Ansi dim = Ansi.grey;
+
+  /// Style used to print emphasized console messages.
+  static Ansi emphasize = Ansi.bold;
+
+  /// Style used to print highlighted console messages.
+  static Ansi highlight = Ansi.yellow;
+
+  /// Style used to print success messages.
+  static Ansi success = Ansi.green;
+}
