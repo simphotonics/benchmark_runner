3984096d373e5b1c27f5ba2ef44968ea7ea9c34c
simphotonics <git@simphotonics.com>
2024-04-11 19:38:36 +0100

    Added class variable innerIter.

--- lib/src/base/score.dart
+++ lib/src/base/score.dart
@@ -2,13 +2,17 @@ import '../utils/stats.dart';
 
 /// Object holding sample stats and the sample generation runtime.
 class Score<T extends num> {
-  Score({
-    required this.runtime,
-    required List<T> sample,
-  }) : stats = Stats(sample);
+  Score(
+      {required this.runtime,
+      required List<T> sample,
+      required this.innerIter})
+      : stats = Stats(sample);
 
   /// Micro-benchmark duration
   final Duration runtime;
+
+  /// Indicates if the a sample point was averaged over [iter] runs.
+  final int innerIter;
 
   /// Scores and score stats (in microseconds).
   final Stats<T> stats;
