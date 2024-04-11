bebf1dc7ad7d442c45d1bf0658b4f739957d11e7
simphotonics <git@simphotonics.com>
2024-04-11 19:39:01 +0100

    Added formatting methods.

--- lib/src/extensions/duration_formatter.dart
+++ lib/src/extensions/duration_formatter.dart
@@ -10,7 +10,7 @@ extension DurationFormatter on Duration {
   /// final d = Duration(days: 1234, hours: 3, minutes: 45, seconds: 10);
   /// print(d.format()); // Prints: 1234d:03h:45m:10s
   /// ```
-  String get hhmmss {
+  String get mmss {
     final days = inDays;
     final hours = inHours - 24 * days;
     final minutes = inMinutes - 60 * hours - 1440 * days;
@@ -32,10 +32,15 @@ extension DurationFormatter on Duration {
     return out.toString();
   }
 
-  String get mmssms {
+  /// Converts the duration into a String with
+  /// format: minutes:seconds:microseconds.
+  ///
+  /// * Omits the entry for minutes if it is zero.
+  /// * Pads integers to width 2.
+  String get ssms {
     final minutes = inMinutes;
     final seconds = inSeconds - 60 * minutes;
-    final microseconds = inMilliseconds - 1000 * seconds - 60000 * minutes;
+    final milliseconds = inMilliseconds - 1000 * seconds - 60000 * minutes;
     final out = StringBuffer('[');
 
     if (minutes > 0) {
@@ -45,8 +50,32 @@ extension DurationFormatter on Duration {
     out.write(seconds.toString().padLeft(2, '0'));
     out.write('s:');
 
-    out.write(microseconds.toString().padLeft(3, '0'));
+    out.write(milliseconds.toString().padLeft(3, '0'));
     out.write('ms]');
+    return out.toString();
+  }
+
+  /// Converts the duration into a String with
+  /// format: seconds:milliseconds:microseconds.
+  ///
+  /// * Omits the entry for seconds if it is zero.
+  /// * Pads integers to width 2.
+  String get msus {
+    final seconds = inSeconds;
+    final milliseconds = inMilliseconds - 1000 * seconds;
+    final microseconds =
+        inMicroseconds - 1000 * milliseconds - 1000000 * seconds;
+    final out = StringBuffer('[');
+
+    if (seconds > 0) {
+      out.write(seconds.toString().padLeft(2, '0'));
+      out.write('s:');
+    }
+    out.write(milliseconds.toString().padLeft(3, '0'));
+    out.write('ms:');
+
+    out.write(microseconds.toString().padLeft(3, '0'));
+    out.write('us]');
     return out.toString();
   }
 }
