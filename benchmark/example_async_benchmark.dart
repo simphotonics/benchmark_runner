8e14008fe8352507f7187df5bdf2c543e7bb1be3
simphotonics <git@simphotonics.com>
2026-09-03 21:11:18 +0100

    Updated examples.

--- benchmark/example_async_benchmark.dart
+++ benchmark/example_async_benchmark.dart
@@ -1,26 +1,29 @@
 // ignore_for_file: unused_local_variable
 
+import 'dart:io';
+
 import 'package:benchmark_runner/benchmark_runner.dart';
 
 /// Returns the value [t] after waiting for [duration].
-Future<T> later<T>(T t, [Duration duration = Duration.zero]) {
-  return Future.delayed(duration, () => t);
+Future<T> later<T>(T t, [Duration duration = Duration.zero]) async {
+  sleep(duration); // Has lower overhead compared to Future.pause.
+  return t;
 }
 
 void main(List<String> args) async {
-  await group('Wait for duration', () async {
+  await asyncGroup('1: Wait for duration', () async {
     await asyncBenchmark('10ms', () async {
       await later<int>(39, Duration(milliseconds: 10));
     });
 
     await asyncBenchmark('5ms', () async {
-      await later<int>(27, Duration(milliseconds: 5));
+      await later<String>('result', Duration(milliseconds: 5));
     }, scoreEmitter: MeanEmitter());
   });
 
-  group('Set', () async {
-    await asyncBenchmark('error test', () {
-      throw ('Thrown in benchmark.');
+  group('2: Set', () {
+    benchmark('error test', () {
+      throw ('Thrown in benchmark: error test.');
     });
 
     benchmark('construct', () {
