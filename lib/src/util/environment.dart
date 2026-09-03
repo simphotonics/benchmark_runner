8ef8405e05f0bf31dce9d352ee9e432e0bee2334
simphotonics <git@simphotonics.com>
2026-09-03 21:26:10 +0100

    Removed marker.

--- lib/src/util/environment.dart
+++ lib/src/util/environment.dart
@@ -1,5 +1,3 @@
-// const String marker = 'Z5GnUZnw7';
-
 const isBenchmarkProcess = bool.fromEnvironment('isBenchmarkProcess');
 const isVerbose = bool.fromEnvironment('isVerbose');
 //const isMonochrome = bool.fromEnvironment('isMonochrome');
